5597340e95ccaf5f64277c21d7e3ffa91803ac74
hamada989 <hamada.issam2002@gmail.com>
2021-04-20 21:36:13 -0400

    basic commands

---
+++ Bot.py
@@ -0,0 +1,81 @@
+# Beetlejuice
+# date 4/20/2021
+# By Mohammed Fakhri
+
+import discord
+import os
+from discord.ext import commands, tasks
+import random
+import json
+
+
+BeetStatus = ["Cuppa War", "KFC WARZONE", "Howerd Stern Simulator", "Boxing Champs", 
+"UFC 4", "Cuppa Killa", "Modern Cuppa Warfare", "Anime Princess Palace 2", "BEET EXILES", "DOD", "Finding Mario Judah"]
+
+if __name__ == "__main__":
+
+     def get_prefix(client, message):
+          with open("Prefixes.json", "r") as f:
+               prefixes = json.load(f)
+
+          if (str(message.guild.id) in prefixes):
+               return prefixes[str(message.guild.id)]
+
+          else:
+               prefixes[str(message.guild.id)] = '!'
+
+               with open("Prefixes.json", 'w') as w:
+                    json.dump(prefixes, w, indent = 4)
+               
+               return prefixes[str(message.guild.id)]
+
+     client = commands.Bot(command_prefix = get_prefix)
+
+     @client.event
+     async def on_guild_join(guild):
+          with open("Prefixes.json", "r") as f:
+               prefixes = json.load(f)
+          
+          prefixes[str(guild.id)] = '!'
+
+          with open("Prefixes.json", 'w') as w:
+               json.dump(prefixes, w, indent = 4)
+
+     @client.event
+     async def on_guild_remove(guild):
+          with open("Prefixes.json", 'r') as f:
+               prefixes = json.load(f)
+          
+          prefixes.pop(str(guild.id))
+
+          with open("Prefixes.json", 'w') as w:
+               json.dump(prefixes, w, indent = 4)
+               
+     @client.event
+     async def on_ready():
+          change_game_status.start()
+          print("Cuppa Army General At Your Service")
+
+     @tasks.loop(minutes = 400)
+     async def change_game_status():
+          await client.change_presence(status=discord.Status.online, activity=discord.Game(random.choice(BeetStatus)))
+             
+     @client.event
+     async def on_command_error(ctx, error):
+          if isinstance(error, commands.MissingRequiredArgument):
+               await ctx.send("More Info Needed")
+
+          elif isinstance(error, commands.MissingPermissions):
+               await ctx.send("Only Cuppa Generals Do This")
+          
+          elif isinstance(error, commands.CommandNotFound):
+               await ctx.send("Command Doesn't Exist Pal")
+
+
+     for filename in os.listdir('./cogs'):
+          if filename.endswith('.py'):
+               client.load_extension(f'cogs.{filename[:-3]}')
+     
+
+     client.run("ODI1NTY3MjM2OTQwNDk2OTI3.YF_zZw.M10yW6QxcW_yOYc-x3IH4Oue0-c")
+
---
+++ cogs/Base.py
@@ -0,0 +1,59 @@
+# Beetlejuice
+# date 4/20/2021
+# By Mohammed Fakhri
+
+import discord
+from discord.ext import commands
+import json
+
+class Basic(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command(aliases=["Hello"])
+    async def Hi(self, ctx):
+        await ctx.send("Hi man")
+
+    @commands.command(aliases=["LordMoe", "lordMoe", " lordmoe", "Lordmoe"])
+    async def love(self, ctx):
+        await ctx.send("My Father and My Creator")
+
+    @commands.command(aliases=["Bye", "bye"])
+    async def _bye(self, ctx):
+        await ctx.send("See ya man")
+
+    @commands.command()
+    @commands.has_permissions(administrator=True, manage_roles=True)  
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True, manage_roles=True)  
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        memberName, memberDisc = member.split("#")
+
+        for banned in banned_users:
+            user = banned.user
+            
+            if(user.name, user.discriminator) == (memberName, memberDisc):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Allowed Back to Cuppa Army {user.mention}')
+                return
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def prefix(self, ctx, prefix):
+        with open("Prefixes.json", "r") as f:
+            prefixes = json.load(f)
+
+        prefixes[str(ctx.guild.id)] = prefix
+
+        with open("Prefixes.json", 'w') as w:
+            json.dump(prefixes, w, indent = 4)
+        
+        await ctx.send("Prefix Changed")
+
+def setup(client):
+    client.add_cog(Basic(client))
